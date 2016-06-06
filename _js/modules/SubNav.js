e23eee63d11c092567d772d9ace725a4afb23ed3
Ian Collins <ikcollins@gmail.com>
2016-06-06 11:33:43 -0400

    update navs to use real SubNav class JS code

---
+++ _js/modules/SubNav.js
@@ -0,0 +1,114 @@
+/*jshint -W032 */ /* ignore unnecessary semicolon */
+var Helper = require('./Helper');
+/**
+ * Sets up a sub navigation for selecting particular DIV elements
+ */
+class SubNav {
+
+    /**
+     * Initialize the sub navigation links
+     */
+    constructor(el, callbackFn) {
+        var _this = this,
+            i,
+            mDiv,
+            urlArr = document.URL.split('#'),
+            url = urlArr[0],
+            hash = window.location.hash,
+            hashItem,
+            subnavCheck,
+            subnav = el; // document.getElementsByTagName('nav')[0]
+
+        this.callback = callbackFn;
+        if (hash !== '') hash = hash.substr(1); // remove '#'
+
+        subnavCheck = subnav.getElementsByTagName('input');
+        this.menuItems = subnav.querySelectorAll('ul a');
+        this.menuDivs = [];
+        this.currentSelect = '';
+        this.toggleLabel = subnav.getElementsByTagName('label');
+
+        if (this.toggleLabel.length > 0) {
+            this.toggleLabel = this.toggleLabel[0];
+        } else {
+            this.toggleLabel = document.createElement('div');
+        }
+        this.toggleLabelDefault = this.toggleLabel.innerHTML;
+
+        if (subnavCheck.length > 0 && subnavCheck[0].type === 'checkbox') {
+            subnavCheck = subnavCheck[0];
+            // FF maintains checked state on reload
+            subnavCheck.checked = false;
+        }
+
+        for (i = 0; i < this.menuItems.length; i++) {
+            // each menu item has a corresponding div
+            // (menuItem.id = div.className)
+            mDiv = document.getElementsByClassName(this.menuItems[i].id)[0];
+            if (this.menuItems[i].id === hash && hash) hashItem = this.menuItems[i];
+            this.menuDivs.push(mDiv);
+            if (mDiv) {
+                Helper.addClass(mDiv, 'subnav-selectable');
+            }
+
+            // click event for menu item
+            this.menuItems[i].onclick = function(e) {
+                var hashval = this.href.substr(url.length);
+                e.preventDefault();
+                if (hashval === '') hashval = url;
+                if (history.pushState) {
+                    history.pushState(null, document.title, hashval);
+                }
+                _this.setMenu(this);
+                if (subnavCheck) subnavCheck.checked = false;
+            };
+        }
+
+        if (hashItem) {
+            this.setMenu(hashItem);
+        } else {
+            // initialize the first menuItem as selected
+            this.setMenu(this.menuItems[0]);
+        }
+    }
+
+    /**
+     * set and unset selected menu items
+     * @param {object} menu item DOM element
+     */
+    setMenu(menuItem) {
+        var i;
+
+        if (this.currentSelect !== menuItem) {
+            if (typeof this.callback === 'function') this.callback(menuItem);
+            for (i = 0; i < this.menuItems.length; i++) {
+                if (this.menuItems[i] === menuItem) {
+                    this.currentSelect = menuItem;
+                    Helper.addClass(menuItem, 'active');
+                    // only change the menu button and div, if a div exists
+                    if (this.menuDivs[i]) {
+                        this.toggleLabel.innerHTML = this.menuItems[i].innerHTML;
+                        this.showMenuDiv(this.menuDivs[i], true);
+                    } else {
+                        this.toggleLabel.innerHTML = this.toggleLabelDefault;
+                    }
+                } else {
+                    Helper.removeClass(this.menuItems[i], 'active');
+                    this.showMenuDiv(this.menuDivs[i]);
+                }
+            }
+        }
+    }
+
+    showMenuDiv(elem, add) {
+        if (elem) {
+            if (add === true) {
+                Helper.addClass(elem, 'subnav-selected');
+            } else {
+                Helper.removeClass(elem, 'subnav-selected');
+            }
+        }
+    }
+};
+
+export default SubNav;
--- _js/navs.js
+++ _js/navs.js
@@ -1,35 +1,14 @@
 /*global document,require*/
 
-var Helper = require('./modules/Helper');
+var Subnav = require('./modules/SubNav');
 
 (function() {
     'use strict';
+    var navs = document.getElementsByTagName('nav'),
+        i;
 
-    var i,
-        len,
-        navs = document.getElementsByTagName('nav');
-
-    for (i = 0, len = navs.length; i < len; i++) {
-        addNavClicks(navs[i]);
+    for (i = 0; i < navs.length; i++) {
+        new Subnav(navs[i]);
     }
-
-    function addNavClicks(nav) {
-        var menuItems = nav.getElementsByTagName('a'),
-            check = nav.getElementsByTagName('input')[0],
-            j;
-        
-        if (check) check.checked = false;
-        for (j = 0; j < menuItems.length; j++) {
-            menuItems[j].addEventListener('click',
-                function(e) {
-                    var active = nav.getElementsByClassName('active')[0];
-                    e.preventDefault();
-                    if (active) Helper.removeClass(active, 'active');
-                    Helper.addClass(this, 'active');
-                }
-            );
-        }
-    }
-
 })();
 
