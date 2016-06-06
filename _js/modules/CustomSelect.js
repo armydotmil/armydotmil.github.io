d251be9fc21aaf24a3e7b7f7d6983f64faed7090
Ian Collins <ikcollins@gmail.com>
2016-06-06 13:05:25 -0400

    select input spans the full width, arrow changes with options open

--- _js/modules/CustomSelect.js
+++ _js/modules/CustomSelect.js
@@ -4,13 +4,13 @@ var Helper = require('./Helper');
 
 class CustomSelect {
     constructor(el) {
-        this.optionBox = document.createElement('div');
+        this.cselect = el;
         this.opened = false;
 
         // overlay spans the select box (prevents direct click on select)
-        this.createOverlayElement(el);
+        this.createOverlayElement();
         // optionBox is the custom popup with select options
-        this.createOptionsElement(el);
+        this.createOptionsElement();
         
         document.body.addEventListener('click', this.hideOptions.bind(this));
     }
@@ -21,33 +21,34 @@ class CustomSelect {
         } else {
             this.opened = false;
         }
-        Helper.toggleClass(this.optionBox, 'show');        
+        Helper.toggleClass(this.cselect, 'show-options');        
     }
 
     hideOptions(e) {
         if (this.opened && e.target.className !== 'custom-overlay') {
-            Helper.removeClass(this.optionBox, 'show');
+            Helper.removeClass(this.optionBox, 'show-options');
             this.opened = false;
         }
     }
 
-    createOverlayElement(cselect) {
+    createOverlayElement() {
         var overlay = document.createElement('div');
 
         overlay.setAttribute('class', 'custom-overlay');
         overlay.addEventListener('click', this.toggleOptions.bind(this));
-        cselect.appendChild(overlay);
+        this.cselect.appendChild(overlay);
     }
 
-    createOptionsElement(cselect) {
-        var select = cselect.getElementsByTagName('select')[0],
-            options = cselect.getElementsByTagName('option'),
+    createOptionsElement() {
+        var select = this.cselect.getElementsByTagName('select')[0],
+            options = this.cselect.getElementsByTagName('option'),
+            optionBox = document.createElement('div'),
             optionUl = document.createElement('ul'),
             optionLi,
             i;
 
-        this.optionBox.setAttribute('class', 'custom-options');
-        this.optionBox.appendChild(optionUl);
+        optionBox.setAttribute('class', 'custom-options');
+        optionBox.appendChild(optionUl);
 
         for (i = 0; i < options.length; i++) {
             optionLi = document.createElement('li');
@@ -59,7 +60,7 @@ class CustomSelect {
             });
             optionUl.appendChild(optionLi);
         }
-        cselect.appendChild(this.optionBox);
+        this.cselect.appendChild(optionBox);
     }
 };
 
