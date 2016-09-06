cda01e50332d830b902e109203580514e31b373c
Johnathan Howard <johnathan.howard.jr@saic.com>
2016-09-06 14:54:55 -0400

    disabling ondragstart for the list items container

--- _js/carousel.js
+++ _js/carousel.js
@@ -7,8 +7,15 @@ var Carousel = require('./modules/Carousel'),
     'use strict';
 
     var stage = document.getElementsByClassName('carousel')[0],
+        items = stage.getElementsByClassName('carousel-items'),
         carousel = new Carousel(stage),
         hammertime = new Hammer(stage);
+        
+    for(var i = 0 ; i < items.length;i++){
+        items[i].ondragstart = function(e) {
+            return false;
+        }
+    }
     
     hammertime.on('swipe', function(ev) {
         carousel.swipe(ev.deltaX);
