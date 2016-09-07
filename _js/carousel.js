19537a1521065297dfbb0fa6d21ec4f46007f066
Johnathan Howard <johnathan.howard.jr@saic.com>
2016-09-07 08:54:54 -0400

    prevent phantom clicking

--- _js/carousel.js
+++ _js/carousel.js
@@ -9,16 +9,22 @@ var Carousel = require('./modules/Carousel'),
     var stage = document.getElementsByClassName('carousel')[0],
         items = stage.getElementsByClassName('carousel-items'),
         carousel = new Carousel(stage),
-        hammertime = new Hammer(stage, {threshold: 2});
-        
-    for(var i = 0 ; i < items.length;i++){
+        hammertime = new Hammer(stage);
+
+    for (var i = 0; i < items.length; i++) {
         items[i].ondragstart = function(e) {
             return false;
         }
+        items[i].onmousedown = function(e) {
+            return false;
+        }
     }
-    
+
     hammertime.on('swipeleft swiperight', function(ev) {
+        //toggle click events to prevent phantom clicking
+        Hammer.off(ev.target, 'click');
         carousel.swipe(ev.deltaX);
+        Hammer.on(ev.target, 'click');
     });
-    
+
 })();
