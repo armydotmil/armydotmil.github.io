c72e012f1e7257a3105ec679ec221d21ca507580
Brett Nolf <brett.m.nolf@saic.com>
2016-11-21 10:40:08 -0500

    added validity js

---
+++ _js/modules/Validity.js
@@ -0,0 +1,82 @@
+/*jshint -W032 */ /* ignore unnecessary semicolon */
+
+var Helper = require('./Helper');
+
+class Validity {
+
+    constructor(form) {
+        var $this = this,
+            field;
+
+        // rewrite checkValidity function if its not available in the browser
+        form.checkValidity = form.checkValidity || function() {
+            var invalid = 0;
+
+            for (var i = 0, len = this.elements.length; i < len; i++) {
+                var el = this.elements[i];
+
+                if (!$this.isValid(el) &&
+                    !Helper.hasClass(el, 'no-validate')) {
+
+                    Helper.addClass(el, 'invalid');
+
+                    invalid++;
+                } else {
+                    if (Helper.hasClass(el, 'invalid'))
+                        Helper.removeClass(el, 'invalid');
+                }
+            }
+
+            if (invalid > 0) return false;
+
+            this.submit();
+        };
+
+        form.onsubmit = function () {
+            if (!this.checkValidity()) return false;
+        };
+    }
+
+    isValid(el) {
+        if (el.getAttribute('type') === 'email') {
+
+            return !this.isEmpty(el) && this.validateEmail(el) && !this.isPlaceholder(el);
+
+        } else if (el.getAttribute('type') === 'tel') {
+
+            return !this.isEmpty(el) && this.validatePhone(el) && !this.isPlaceholder(el);
+
+        } else if (el.getAttribute('type') === 'url') {
+
+            return !this.isEmpty(el) && this.validateUrl(el) && !this.isPlaceholder(el);
+
+        } else {
+            return !this.isEmpty(el) && !this.isPlaceholder(el);
+        }
+    }
+
+    validateEmail(el) {
+        // http://code.tutsplus.com/tutorials/8-regular-expressions-you-should-know--net-6149
+        return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(el.value);
+    }
+
+    validatePhone(el) {
+        return /^(?:\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}$/.test(el.value);
+    }
+
+    validateUrl(el) {
+        // http://code.tutsplus.com/tutorials/8-regular-expressions-you-should-know--net-6149
+        return /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/gi.test(el.value);
+    }
+
+    isEmpty(el) {
+        return el.value.length === 0 || !el.value.trim();
+    }
+
+    isPlaceholder(el) {
+        return el.getAttribute('placeholder') && el.value !== el.getAttribute('placeholder');
+    }
+
+};
+
+export default Validity;
