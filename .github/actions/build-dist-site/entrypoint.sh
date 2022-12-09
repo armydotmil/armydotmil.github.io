48e2fd4369177467fc7ca6e317f0a7567b33f7f5
Ian Collins <ikcollins@gmail.com>
2022-12-09 12:30:47 -0500

    try using entrypoint from jekyll-build-pages with system update and bundle install

--- .github/actions/build-dist-site/entrypoint.sh
+++ .github/actions/build-dist-site/entrypoint.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
-# Exit immediately if a pipeline returns a non-zero status.
-set -e
 
-echo "🚀 Starting deployment action"
+#######################################################################################################
+#
+# Calls github-pages executable to build the site using whitelisted plugins and supported configuration
+#
+#######################################################################################################
 
-# Here we are using the variables
-# - GITHUB_ACTOR: It is already made available for us by Github. It is the username of whom triggered the action
-# - GITHUB_TOKEN: That one was intentionally injected by us in our workflow file.
-# Creating the repository URL in this way will allow us to `git push` without providing a password
-# All thanks to the GITHUB_TOKEN that will grant us access to the repository
-REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+set -o errexit
 
-# We need to clone the repo here.
-# Remember, our Docker container is practically pristine at this point
-git clone $REMOTE_REPO repo
-cd repo
+SOURCE_DIRECTORY=${GITHUB_WORKSPACE}/$INPUT_SOURCE
+DESTINATION_DIRECTORY=${GITHUB_WORKSPACE}/$INPUT_DESTINATION
+PAGES_GEM_HOME=$BUNDLE_APP_CONFIG
+GITHUB_PAGES=$PAGES_GEM_HOME/bin/github-pages
+
+# Set environment variables required by supported plugins
+export JEKYLL_ENV="production"
+export JEKYLL_GITHUB_TOKEN=$INPUT_TOKEN
+export PAGES_REPO_NWO=$GITHUB_REPOSITORY
+export JEKYLL_BUILD_REVISION=$INPUT_BUILD_REVISION
+
+# Set verbose flag
+if [ "$INPUT_VERBOSE" = 'true' ]; then
+  VERBOSE='--verbose'
+else
+  VERBOSE=''
+fi
+
+# Set future flag
+if [ "$INPUT_FUTURE" = 'true' ]; then
+  FUTURE='--future'
+else
+  FUTURE=''
+fi
+
+cd "$PAGES_GEM_HOME"
 
 # Install latest version of RubyGems bundler
 echo "⚡️ Installing RubyGems bundler..."
@@ -27,33 +46,5 @@ bundle install
 
 # Build the website using Jekyll
 echo "🏋️ Building website..."
-JEKYLL_ENV=production bundle exec jekyll build
+$GITHUB_PAGES build "$VERBOSE" "$FUTURE" --source "$SOURCE_DIRECTORY" --destination "$DESTINATION_DIRECTORY"
 echo "Jekyll build done"
-
-# Now lets go to the generated folder by Jekyll
-# and perform everything else from there
-cd _site
-
-echo "☁️ Publishing website"
-
-# We don't need the README.md file on this branch
-rm -f README.md
-
-# Now we init a new git repository inside _site
-# So we can perform a commit
-git init
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-git add .
-# That will create a nice commit message with something like:
-# Github Actions - Fri Sep 6 12:32:22 UTC 2019
-git commit -m "Github Actions - $(date)"
-echo "Build branch ready to go. Pushing to Github..."
-# Force push this update to our gh-pages
-git push --force $REMOTE_REPO master:gh-pages
-# Now everything is ready.
-# Lets just be a good citizen and clean-up after ourselves
-rm -fr .git
-cd ..
-rm -rf repo
-echo "🎉 New version deployed 🎊"
