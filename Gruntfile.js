f887ffce04d4455abc4b0799c29ee611fe5274d6
Ian Collins <ikcollins@gmail.com>
2023-12-20 14:13:09 -0500

    have to make some change to trigger a push event in github

--- Gruntfile.js
+++ Gruntfile.js
@@ -126,7 +126,6 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-browserify');
-    // grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
